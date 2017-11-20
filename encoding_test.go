602938644c83804ce9ed84353dd12e8e6bc8657d
Michael Woolnough <michael.woolnough@gmail.com>
2017-11-20 16:39:10 +0000

    added tests

---
+++ encoding_test.go
@@ -0,0 +1,40 @@
+package httpencoding
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+type testEncodings []string
+
+func (t *testEncodings) Handle(encoding string) bool {
+	*t = append(*t, encoding)
+	return false
+}
+
+func TestOrder(t *testing.T) {
+	for n, test := range []struct {
+		AcceptEncoding string
+		Encodings      testEncodings
+	}{
+		{"", testEncodings{""}},
+		{"gzip", testEncodings{"gzip", ""}},
+		{"gzip, deflate, br", testEncodings{"gzip", "deflate", "br", ""}},
+		{"gzip, deflate;q=0.5, br;q=1.9", testEncodings{"br", "gzip", "deflate", ""}},
+		{"identity, gzip, deflate;q=0.5, br;q=1.9", testEncodings{"br", "", "gzip", "deflate"}},
+		{"gzip, br, identity;q=0", testEncodings{"gzip", "br"}},
+		{"gzip, br, identity;q=0, *", testEncodings{"gzip", "br"}},
+		{"gzip, *, br", testEncodings{"gzip", "", "br"}},
+	} {
+		te := make(testEncodings, 0, len(test.Encodings))
+		HandleEncoding(&http.Request{
+			Header: http.Header{
+				acceptEncoding: []string{test.AcceptEncoding},
+			},
+		}, &te)
+		if !reflect.DeepEqual(te, test.Encodings) {
+			t.Errorf("test %d: expecting %v, got %v", n+1, test.Encodings, te)
+		}
+	}
+}
