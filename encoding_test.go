6746d463f86b197867401f60a9049909753316ae
Michael Woolnough <michael.woolnough@gmail.com>
2025-09-12 08:34:51 +0100

    update tests to handle wildcards correctly

--- encoding_test.go
+++ encoding_test.go
@@ -10,6 +10,7 @@ type testEncodings []string
 
 func (t *testEncodings) Handle(encoding Encoding) bool {
 	*t = append(*t, string(encoding))
+
 	return false
 }
 
@@ -18,21 +19,23 @@ func TestOrder(t *testing.T) {
 		AcceptEncoding string
 		Encodings      testEncodings
 	}{
-		{"", testEncodings{""}},
+		{"", testEncodings{"*;", ""}},
 		{"gzip", testEncodings{"gzip", ""}},
 		{"gzip, deflate, br", testEncodings{"gzip", "deflate", "br", ""}},
-		{"gzip, deflate;q=0.5, br;q=1.9", testEncodings{"br", "gzip", "deflate", ""}},
-		{"identity, gzip, deflate;q=0.5, br;q=1.9", testEncodings{"br", "", "gzip", "deflate"}},
+		{"gzip, deflate;q=0.5, br;q=0.9", testEncodings{"gzip", "br", "deflate", ""}},
+		{"identity, gzip, deflate;q=0.5, br;q=0.9", testEncodings{"", "gzip", "br", "deflate"}},
 		{"gzip, br, identity;q=0", testEncodings{"gzip", "br"}},
-		{"gzip, br, identity;q=0, *", testEncodings{"gzip", "br"}},
-		{"gzip, *, br", testEncodings{"gzip", "", "br"}},
+		{"gzip, br, identity;q=0, *", testEncodings{"gzip", "br", "*;;"}},
+		{"gzip, *, br", testEncodings{"gzip", "*;", "br", ""}},
 	} {
 		te := make(testEncodings, 0, len(test.Encodings))
+
 		HandleEncoding(&http.Request{
 			Header: http.Header{
 				acceptEncoding: []string{test.AcceptEncoding},
 			},
 		}, &te)
+
 		if !reflect.DeepEqual(te, test.Encodings) {
 			t.Errorf("test %d: expecting %v, got %v", n+1, test.Encodings, te)
 		}
